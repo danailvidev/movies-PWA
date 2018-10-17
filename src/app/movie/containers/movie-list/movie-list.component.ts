78a738e66c237712a468a3d30568863da90ee93e
Danail Videv <danailvidev@gmail.com>
2018-10-17 12:28:40 +0300

    movies list

--- src/app/movie/containers/movie-list/movie-list.component.ts
+++ src/app/movie/containers/movie-list/movie-list.component.ts
@@ -1,19 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Movie } from '../../models/movie';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-movie-list',
-    template: `
-        {{movies | json}}
-  `,
-    styles: []
+    templateUrl: 'movies-list.component.html',
+    styleUrls: ['movies-list.component.scss']
 })
 export class MovieListComponent implements OnInit {
     public movies: Movie[];
+    subscription: Subscription = new Subscription();
 
-    constructor() { }
+    constructor(private http:HttpClient) { }
 
     ngOnInit() {
+       this.getMovies(); 
+    }
+
+    getMovies() {
+        this.subscription.add(
+            this.http.get<any>('http://www.omdbapi.com/?apikey=fee8d867&y=2017&t=sex').subscribe(res => {
+                console.log(res);
+                this.movies = res;
+            })
+        )
     }
 
 }
--- src/app/movie/movie.module.ts
+++ src/app/movie/movie.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { MovieRoutingModule } from './movie-routing.module';
 import { MovieListComponent } from './containers/movie-list/movie-list.component';
 import { MovieDetailComponent } from './containers/movie-detail/movie-detail.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     CommonModule,
-    MovieRoutingModule
+    MovieRoutingModule,
+    HttpClientModule
   ],
   declarations: [MovieListComponent, MovieDetailComponent]
 })
